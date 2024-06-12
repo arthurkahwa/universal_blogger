039cd75045560a08e836ac321a7886bce501c078
arthurkahwa <33757012+arthurkahwa@users.noreply.github.com>
2024-06-12 21:59:20 +0200

    sql inserts

--- utilities/json2sql.py
+++ utilities/json2sql.py
@@ -1,14 +1,15 @@
 import json
 
-root = '01_users'
+root = '03_albums'
 file_fp = open('../postgres/data/' + root + '.json')
 json_entries = json.load(file_fp)
-sql_insert_template = 'insert into users (id, username) values (_id, _username)'
+sql_insert_template = 'insert into albums (id, userid, title) values (_id, _userid, _title)'
 file_fp.close()
 sql_fp = open('../postgres/' + root + '.sql', 'w')
 for entry in json_entries:
     sql = sql_insert_template.replace('_id', str(entry['id']))
-    #sql = sql.replace('_userid', str(entry['userId']))
-    sql = sql.replace('_username', "'" + entry['email'] + "'")
+    sql = sql.replace('_userid', str(entry['userId']))
+    sql = sql.replace('_title', "'" + entry['title'] + "'")
     sql_fp.write(sql + ';\n')
     print(sql)
+sql_fp.close()
